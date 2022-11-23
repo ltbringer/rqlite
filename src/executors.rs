c3db4ce0b964ed37638d3a06a364e028e5d3629f
ltbringer <amresh.venugopal@gmail.com>
2022-11-24 02:54:04 +0530

    update: select and insert commands over mock db.

--- src/executors.rs
+++ src/executors.rs
@@ -1,17 +1,26 @@
 use std::process;
-use super::commands::{QueryType, MetaCommands};
-
-pub fn exec_query(query: QueryType) -> String {
-    match query {
-        QueryType::SELECT => "Select columns from table.".to_string(),
-        QueryType::INSERT => "Insert into table.".to_string(),
-        _ => "SQL Parsing failed.".to_string()
-    }
-}
+use super::commands::{QueryType, MetaCommands, Query};
+use super::mock::db;
 
 pub fn exec_cmd(cmd: MetaCommands) -> () {
     match cmd {
         MetaCommands::EXIT => process::exit(0),
         MetaCommands::UNKNOWN => ()
+    }
+}
+
+pub fn exec_query(q: Query) -> String {
+    let mut db_ = db().to_vec();
+    match q.command {
+        QueryType::SELECT => {
+            return format!("{:?}", db_);
+        },
+        QueryType::INSERT => {
+            let last = *db_.last().unwrap();
+            let last_id = last.0;
+            db_.push((last_id + 1, "...", 18, 12, 50_000));
+            return format!("{:?}", db_);
+        },
+        _ => "SQL Parsing failed.".to_string()
     }
 }
