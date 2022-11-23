eadf666948e290db2db2bcc643bcc98c019883a5
ltbringer <amresh.venugopal@gmail.com>
2022-11-24 02:53:45 +0530

    update: skeleton for query struct.

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,4 +1,4 @@
-use super::commands::{MetaCommands, QueryType};
+use super::commands::{MetaCommands, QueryType, Query};
 
 pub fn parse_command(command: &str) -> MetaCommands {
     if command == ".exit" {
@@ -7,11 +7,36 @@ pub fn parse_command(command: &str) -> MetaCommands {
     MetaCommands::UNKNOWN
 }
 
-pub fn parse_query(query: &str) -> QueryType {
-    if query.to_lowercase() == "select" {
-        return QueryType::SELECT
-    } else if query.to_lowercase() == "insert" {
-        return QueryType::INSERT
+pub fn parse_query(query: &str) -> Query {
+    let q_parts: Vec<&str> = query.split_whitespace().collect();
+    let err_q = Query {
+        command: QueryType::ERROR,
+        columns: ("".to_string(),),
+        table: "".to_string(),
+        values: ("".to_string(),)
+    };
+
+    if q_parts.len() == 0 {
+        return err_q
     }
-    return QueryType::ERROR
+
+    let cmd = q_parts[0].to_lowercase();
+
+    if cmd == "select" {
+        return Query {
+            command: QueryType::SELECT,
+            columns: ("".to_string(),),
+            table: "".to_string(),
+            values: ("".to_string(),)
+        }
+    } else if cmd == "insert" {
+        return Query {
+            command: QueryType::INSERT,
+            columns: ("".to_string(),),
+            table: "".to_string(),
+            values: ("".to_string(),)
+        }
+    }
+
+    return err_q
 }
