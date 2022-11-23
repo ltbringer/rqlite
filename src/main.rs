8ec0da91f3037b546b99422a3919a72992065edc
ltbringer <amresh.venugopal@gmail.com>
2022-11-24 01:32:10 +0530

    refactor: move all fn to mods.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use std::io::{stdout, stdin, Write};
+mod commands;
+mod parsers;
+mod executors;
+mod repl;
 
-fn neat_query(strings: Vec<String>) -> String {
-    let clean_strings = strings.iter()
-        .map(|s| adjust_whitespace(s))
-        .collect::<Vec<_>>();
-    clean_strings.join(" ")
-}
-
-fn adjust_whitespace(text: &str) -> String {
-    let texts: Vec<_> = text.split_whitespace().collect();
-    texts.join(" ")
-}
+use repl::repl;
 
 fn main() {
-    let prompt: &str = "sqlite> ";
-    let mut lock = stdout().lock();
-    loop {
-        write!(lock, "{}", prompt).unwrap();
-        stdout().flush().expect("Flush failed");
-        let lines = stdin().lines();
-        let raw_query = lines
-            .map(|x| x.unwrap())
-            .collect::<Vec<_>>();
-        let query = neat_query(raw_query);
-        write!(lock, "{}\n", query).unwrap();
-    }
+    repl();
 }
