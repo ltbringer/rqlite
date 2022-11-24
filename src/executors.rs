ee79065861a89a685455136de89b1d071900d475
ltbringer <amresh.venugopal@gmail.com>
2022-11-25 00:34:51 +0530

    update: select columns by name.

--- src/executors.rs
+++ src/executors.rs
@@ -9,11 +9,31 @@ pub fn exec_cmd(cmd: MetaCommands) -> () {
     }
 }
 
+fn select<'a>(cols: Vec<String>, db: &'a mut Vec<RecordFactory>) -> Vec<RecordFactory<'a>> {
+    db.iter().map(|r| {
+        let mut record = RecordFactory::new();
+        for column in cols.iter() {
+            match r.get(&column).unwrap() {
+                TDTypes::Int(v) => record.set(&column, TDTypes::Int(*v)),
+                TDTypes::Str(v) => record.set(&column, TDTypes::Str(*v))
+            };
+        }
+        record
+    }).collect::<Vec<RecordFactory>>()
+}
+
 pub fn exec_query(q: Query) -> String {
-    let mut db_ = db().to_vec();
+    let mut db_ = db();
     match q.command {
         QueryType::SELECT => {
-            return format!("{:?}", db_);
+            if let Some(c) = q.columns {
+                if (c.len() == 1 && c[0] == "*") || (c.len() == 3) {
+                    return format!("{:#?}", db_);
+                } else {
+                    return format!("{:#?}", select(c, &mut db_))
+                }
+            }
+            return format!("{:#?}", db_);
         },
         QueryType::INSERT => {
             let last = *db_.last().unwrap();
