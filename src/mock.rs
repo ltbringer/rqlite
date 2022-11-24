9264a9a4935b2a83b6c85534f1fc6cee0d4eab04
ltbringer <amresh.venugopal@gmail.com>
2022-11-25 00:33:37 +0530

    update: impl db via hashmap.

--- src/mock.rs
+++ src/mock.rs
@@ -1,15 +1,41 @@
-type Tid = u32;
-type TName<'a> = &'a str;
-type TAge = u8;
-type TEmpCode = u8;
-type TComp = u32;
-type Trow<'a> = (Tid, TName<'a>, TAge, TEmpCode, TComp);
+use std::collections::HashMap;
 
-pub fn db<'a>() -> &'static [Trow<'a>] {
-    &[
-        (1, "Jack", 20, 3, 36_000),
-        (2, "Molly", 23, 8, 76_000),
-        (3, "Steve", 27, 18, 56_000),
-        (4, "Pete", 33, 1, 126_000)
-    ]
+#[derive(Debug, Clone)]
+pub enum TDTypes <'a> {
+    Str(&'a str),
+    Int(u32)
+}
+
+#[derive(Debug, Clone)]
+pub struct RecordFactory <'a> {
+    pub m: HashMap<String, TDTypes<'a>>
+}
+
+impl<'a> RecordFactory <'a> {
+    pub fn new() -> Self {
+        RecordFactory { m: HashMap::new() }
+    }
+
+    pub fn set(&mut self, k: &str, v: TDTypes<'a>) -> &mut Self {
+        self.m.insert(String::from(k), v);
+        return self;
+    }
+
+    pub fn get(&self, k: &str) -> Option<&TDTypes> {
+        self.m.get(k)
+    }
+}
+
+pub fn db<'a> () -> Vec<RecordFactory<'a>> {
+    let r1 = RecordFactory::new()
+        .set("id", TDTypes::Int(1))
+        .set("name", TDTypes::Str("Jack"))
+        .set("salary", TDTypes::Int(35000))
+        .clone();
+    let r2 = RecordFactory::new()
+        .set("id", TDTypes::Int(2))
+        .set("name", TDTypes::Str("Molly"))
+        .set("salary", TDTypes::Int(45000))
+        .clone();
+    vec![r1, r2]
 }
