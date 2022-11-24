8ce6024c78bcf98cc09411772d36f395ad0435fd
ltbringer <amresh.venugopal@gmail.com>
2022-11-25 00:34:12 +0530

    update: cols and values are vec<str>.

--- src/parsers.rs
+++ src/parsers.rs
@@ -7,13 +7,24 @@ pub fn parse_command(command: &str) -> MetaCommands {
     MetaCommands::UNKNOWN
 }
 
+pub fn own_str_vec(parts: &Vec<&str>, start: usize, end: Option<usize>) -> Vec<String> {
+    let _end = match end {
+        Some(e) => e,
+        None => parts.len()
+    };
+    String::from(&parts[start.._end].join(" "))
+            .split_whitespace()
+            .map(|word| word.to_owned())
+            .collect::<Vec<String>>()
+}
+
 pub fn parse_query(query: &str) -> Query {
     let q_parts: Vec<&str> = query.split_whitespace().collect();
     let err_q = Query {
         command: QueryType::ERROR,
-        columns: ("".to_string(),),
-        table: "".to_string(),
-        values: ("".to_string(),)
+        columns: None,
+        table: None,
+        values: None
     };
 
     if q_parts.len() == 0 {
@@ -23,18 +34,28 @@ pub fn parse_query(query: &str) -> Query {
     let cmd = q_parts[0].to_lowercase();
 
     if cmd == "select" {
+        // select * from table
+        let sep = q_parts.iter().position(|&word| word == "from").unwrap();
+        let table = q_parts[sep + 1].to_owned();
+        let columns = own_str_vec(&q_parts, 1, Some(sep));
         return Query {
             command: QueryType::SELECT,
-            columns: ("".to_string(),),
-            table: "".to_string(),
-            values: ("".to_string(),)
+            columns: Some(columns),
+            table: Some(table),
+            values: None
         }
     } else if cmd == "insert" {
+        // insert into table name salary values bob 36000
+        let table = q_parts[2].to_owned();
+        let sep = q_parts.iter().position(|&word| word == "values").unwrap() + 1;
+        let columns = own_str_vec(&q_parts, 3, Some(sep - 1));
+        let values = own_str_vec(&q_parts, sep, None);
+
         return Query {
             command: QueryType::INSERT,
-            columns: ("".to_string(),),
-            table: "".to_string(),
-            values: ("".to_string(),)
+            columns: Some(columns),
+            table: Some(table),
+            values: Some(values)
         }
     }
 
