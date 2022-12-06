600c838cdfd3c4060d5816888e7f87306ef4b0cf
ltbringer <amresh.venugopal@gmail.com>
2022-12-07 03:23:18 +0530

    add: a mock db and table for cli.

--- src/repl.rs
+++ src/repl.rs
@@ -1,11 +1,18 @@
 use std::io::{stdout, stdin, Write};
 use super::commands::parser::Parser;
 use super::commands::exec::run_command;
+use super::commands::types::{Db, Table, DTypes};
 
 pub fn repl() {
     let prompt: &str = "sqlite> ";
     let mut lock = stdout().lock();
     let parser = Parser::build();
+    let mut table = Table::new(String::from("people"));
+    let mut db = Db::new();
+    table.set_col("name", DTypes::VARCHAR)
+        .set_col("age", DTypes::INT)
+        .set_col("is_alive", DTypes::BOOL);
+    db.add("people", table);
 
     loop {
         let mut raw_cmd = String::new();
@@ -21,6 +28,6 @@ pub fn repl() {
 
         raw_cmd = String::from(raw_cmd.trim());
         let cmd = parser.parse(raw_cmd);
-        run_command(cmd);
+        run_command(cmd, &mut db);
     }
 }
