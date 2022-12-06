a9bfd89e2a53c10c5c0ee822ac3433d554521bc2
ltbringer <amresh.venugopal@gmail.com>
2022-12-07 03:22:38 +0530

    update: serialize table page data.

--- src/commands/types.rs
+++ src/commands/types.rs
@@ -1,4 +1,6 @@
+use bincode;
 use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
 
 pub type TableRowsCtr = u64;
 pub type TableColsCtr = u8;
@@ -10,10 +12,11 @@ pub enum MetaCommand {
     INVALIDQUERY
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum DTypes {
     INT,
-    VARCHAR
+    VARCHAR,
+    BOOL
 }
 
 #[derive(Debug)]
@@ -36,24 +39,86 @@ pub enum Command {
     Q(Query)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MockSchema {
+    name: String,
+    age: u8,
+    is_alive: bool
+}
+
+impl MockSchema {
+    pub fn new(name: String, age: u8, is_alive: bool) -> Self {
+        MockSchema { name, age, is_alive }
+    }
+}
+
+fn deserialize_row(page: &Vec<u8>) -> Option<MockSchema> {
+    let data = bincode::deserialize(&page);
+    match data {
+        Ok(row) => Some(row),
+        Err(e) => {
+            println!("{:?}", e);
+            None
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Table {
+    name: String,
     n_cols: TableColsCtr,
     n_rows: TableRowsCtr,
-    name: String,
-    col_names: Vec<String>,
-    col_types: Vec<DTypes>,
+    cols: Vec<(String, DTypes)>,
+    pub pages: Vec<Vec<u8>>
+}
+
+impl Table {
+    pub fn new(name: String) -> Self {
+        Table {
+            name,
+            n_cols: 0,
+            n_rows: 0,
+            cols: vec![],
+            pages: vec![]
+        }
+    }
+
+    pub fn set_col(&mut self, col_name: &str, col_type: DTypes) -> &mut Self {
+        self.cols.push((String::from(col_name), col_type));
+        self.n_cols += 1;
+        self
+    }
+
+    pub fn insert<T: serde::ser::Serialize>(&mut self, data: T) -> &mut Self {
+        let row_op = bincode::serialize(&data);
+        match row_op {
+            Ok(row) => {
+                self.pages.push(row);
+                self.n_rows += 1;
+            },
+            Err(e) => { println!("{:?}", e); }
+        }
+        self
+    }
+
+    pub fn select(&self) -> Vec<MockSchema> {
+        self.pages.iter()
+            .map(deserialize_row)
+            .filter(|x| x.is_some())
+            .map(|x| x.unwrap())
+            .collect::<Vec<_>>()
+    }
 }
 
 pub struct Db {
-    tables: HashMap<String, Table>,
+    pub tables: HashMap<String, Table>,
     n_tables: u64
 }
 
 impl Db {
-    pub fn get<'a>(self, name: &str) -> Option<&'a Table> {
-        self.tables.get(name)
-    }
+    // pub fn get<'a>(self, name: &str) -> Option<&'a Table> {
+    //     self.tables.get(name)
+    // }
 
     pub fn new() -> Self {
         let tables = HashMap::new();
@@ -61,7 +126,8 @@ impl Db {
     }
 
     pub fn add(&mut self, table_name: &str, table: Table) -> &mut Self {
-        self.tables.insert(String::from(table_name), table);
+        let k = String::from(table_name);
+        self.tables.insert(k, table);
         self.n_tables += 1;
         self
     }
